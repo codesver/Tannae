ac097af1bb7137bdcbce243ded56de1291f04c5a
JaeWon Choi <unsyg.choi@gmail.com>
2022-08-20 14:53:08 +0900

    [Update] Client 202208201452
    - activity_sign_up

--- tannae-client/app/src/main/java/codesver/tannae/activity/LoginActivity.java
+++ tannae-client/app/src/main/java/codesver/tannae/activity/LoginActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -12,8 +11,8 @@ import codesver.tannae.R;
 
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText editIdView, editPwView;
-    private Button buttonFindView, buttonSignUpView, buttonLoginView;
+    private EditText editId, editPw;
+    private Button buttonFind, buttonSignUp, buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +23,16 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void setViews() {
-        editIdView = findViewById(R.id.edit_id_login);
-        editPwView = findViewById(R.id.edit_pw_login);
-        buttonFindView = findViewById(R.id.button_find_login);
-        buttonSignUpView = findViewById(R.id.button_sign_up_login);
-        buttonLoginView = findViewById(R.id.button_login_login);
+        editId = findViewById(R.id.edit_id_login);
+        editPw = findViewById(R.id.edit_pw_login);
+        buttonFind = findViewById(R.id.button_find_login);
+        buttonSignUp = findViewById(R.id.button_sign_up_login);
+        buttonLogin = findViewById(R.id.button_login_login);
     }
 
     private void setEventListeners() {
-        buttonFindView.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), FindActivity.class)));
-        buttonSignUpView.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
-        buttonLoginView.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), MainActivity.class)));
+        buttonFind.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), FindActivity.class)));
+        buttonSignUp.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+        buttonLogin.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), MainActivity.class)));
     }
 }
--- tannae-client/app/src/main/java/codesver/tannae/activity/SignUpActivity.java
+++ tannae-client/app/src/main/java/codesver/tannae/activity/SignUpActivity.java
@@ -1,16 +1,54 @@
 package codesver.tannae.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import codesver.tannae.R;
 
 public class SignUpActivity extends AppCompatActivity {
+    private Button buttonBack, buttonCheckId, buttonCheckUser, buttonSignUp;
+    private EditText editId, editPw, editPwCheck, editName, editRrnFront, editRrnBack, editEmail, editPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        setViews();
+        setEventListeners();
+    }
+
+    private void setViews() {
+        buttonBack = findViewById(R.id.button_back_sign_up);
+        buttonCheckId = findViewById(R.id.button_check_id_sign_up);
+        buttonCheckUser = findViewById(R.id.button_check_user_sign_up);
+        buttonSignUp = findViewById(R.id.button_sign_up_sign_up);
+
+        editId = findViewById(R.id.edit_id_sign_up);
+        editPw = findViewById(R.id.edit_pw_sign_up);
+        editPwCheck = findViewById(R.id.edit_pw_check_sign_up);
+        editName = findViewById(R.id.edit_name_sign_up);
+        editRrnFront = findViewById(R.id.edit_rrn_front_sign_up);
+        editRrnBack = findViewById(R.id.edit_rrn_back_sign_up);
+        editEmail = findViewById(R.id.edit_email_sign_up);
+        editPhone = findViewById(R.id.edit_phone_sign_up);
+    }
+
+    private void setEventListeners() {
+        buttonBack.setOnClickListener(v -> onBackPressed());
+        buttonCheckId.setOnClickListener(v -> checkId());
+        buttonCheckUser.setOnClickListener(v -> checkUser());
+        buttonSignUp.setOnClickListener(v -> signUp());
+    }
+
+    private void checkId() {
+    }
+
+    private void checkUser() {
+    }
+
+    private void signUp() {
     }
 }
