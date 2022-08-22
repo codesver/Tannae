5a204c280f1d64ffb86d93aec5a5cee278ecc266
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-08-22 12:43:00 +0900

    [Update] Server 202208221242
    - Change log structure

--- tannae-server/src/main/java/codesver/tannae/controller/UserController.java
+++ tannae-server/src/main/java/codesver/tannae/controller/UserController.java
@@ -16,19 +16,19 @@ public class UserController {
 
     @GetMapping("/check-id")
     public Boolean checkId(@RequestParam String id) {
-        log.info("[CONTROLLER-USER-CHECK_ID] /users/check-id?id={}", id);
+        log.info("[CONTROLLER-USER : CHECK_ID] /users/check-id?id={}", id);
         return signUpService.isAvailableId(id);
     }
 
     @GetMapping("/check-private")
     public Boolean checkPrivate(@RequestParam String name, @RequestParam String rrn) {
-        log.info("[CONTROLLER-USER-CHECK_PRIVATE] /users/check-private?name={}&rrn={}", name, rrn);
+        log.info("[CONTROLLER-USER : CHECK_PRIVATE] /users/check-private?name={}&rrn={}", name, rrn);
         return signUpService.isAvailableUser(name, rrn);
     }
 
     @PostMapping("/sign-up")
     public Boolean signUp(@RequestBody SignUpUserDTO dto) {
-        log.info("[CONTROLLER-USER-SIGN_UP] /users/sign-up body={}", dto);
+        log.info("[CONTROLLER-USER : SIGN_UP] /users/sign-up body={}", dto);
         return signUpService.signUpSuccessfully(dto);
     }
 }
--- tannae-server/src/main/java/codesver/tannae/repository/user/UserJpaRepository.java
+++ tannae-server/src/main/java/codesver/tannae/repository/user/UserJpaRepository.java
@@ -16,21 +16,21 @@ public class UserJpaRepository implements UserRepository {
     @Override
     @Transactional
     public int countById(String id) {
-        log.info("[REPOSITORY-USER-COUNT_BY_ID] Counting user by id={}", id);
+        log.info("[REPOSITORY-USER : COUNT_BY_ID] Counting user by id={}", id);
         return repository.countUserById(id);
     }
 
     @Override
     @Transactional
     public int countByPrivate(String name, String rrn) {
-        log.info("[REPOSITORY-USER-COUNT_BY_PRIVATE] Counting user by name={} rrn={}", name, rrn);
+        log.info("[REPOSITORY-USER : COUNT_BY_PRIVATE] Counting user by name={} rrn={}", name, rrn);
         return repository.countUserByRrn(rrn);
     }
 
     @Override
     @Transactional
     public boolean save(User user) {
-        log.info("[REPOSITORY-USER-SAVE] Save user={}", user);
+        log.info("[REPOSITORY-USER : SAVE] Save user={}", user);
         repository.save(user);
         return true;
     }
