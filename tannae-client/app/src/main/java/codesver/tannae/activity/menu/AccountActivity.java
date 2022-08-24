99a1659fe5af7022bf787eaa4249acac2d6c4064
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-08-24 15:37:01 +0900

    [Update] Client 202208241536
    - Create account activity

---
+++ tannae-client/app/src/main/java/codesver/tannae/activity/menu/AccountActivity.java
@@ -0,0 +1,16 @@
+package codesver.tannae.activity.menu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import codesver.tannae.R;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
