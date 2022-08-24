37ef45cdffaa087302b7addef748197a5e1b1ef7
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-08-24 17:37:49 +0900

    [Update] Client 202208241737
    - Change IP

--- tannae-client/app/src/main/java/codesver/tannae/network/RetrofitClient.java
+++ tannae-client/app/src/main/java/codesver/tannae/network/RetrofitClient.java
@@ -7,7 +7,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private final static String BASE_URL = "http://118.219.190.205:8080/"; //"http://168.131.175.67:8080/";
+    private final static String BASE_URL = "http://168.131.175.58:8080/"; //"http://118.219.190.205:8080/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
