7825dad7cba59184c778bc7e17253e4640039eaa
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-08-28 20:15:18 +0900

    [Update] Server 202208282015
    - etc

--- tannae-server/src/main/java/codesver/tannae/controller/ServiceController.java
+++ tannae-server/src/main/java/codesver/tannae/controller/ServiceController.java
@@ -3,13 +3,21 @@ package codesver.tannae.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/service")
 public class ServiceController {
 
     private final SimpMessageSendingOperations smso;
+
+    @PostMapping("/request")
+    public void request() {
+        // 이용 가능한 차량이 있으면 차량 정보(vsn 등)을 반환하고 client 는 이를 바탕으로 STOMP 구독
+    }
 
     @MessageMapping("/hello")
     public void message() {
