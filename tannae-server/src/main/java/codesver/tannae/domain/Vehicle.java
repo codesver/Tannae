273e6854b4a2671fd6b4e2b5ba92b306249e6c7f
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-08-29 14:54:11 +0900

    [Update] Server 202208291454
    - Create Vehicle table

---
+++ tannae-server/src/main/java/codesver/tannae/domain/Vehicle.java
@@ -0,0 +1,45 @@
+package codesver.tannae.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer vsn;
+
+    @Column(length = 10, nullable = false)
+    private String vrn;
+
+    @Column(nullable = false)
+    private Double longitude;
+
+    @Column(nullable = false)
+    private Double latitude;
+
+    @Column(nullable = false)
+    private Integer num;
+
+    @Column(length = 1, columnDefinition = "TINYINT", nullable = false)
+    private Boolean run;
+
+    @Column(length = 1, columnDefinition = "TINYINT")
+    private Boolean share;
+
+    @Column(length = 1, columnDefinition = "TINYINT")
+    private Boolean gender;
+
+    @Column
+    @OneToOne
+    @JoinColumn(name = "usn")
+    private User user;
+}
