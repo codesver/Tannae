0de6c2761c10990e8b29c66798307cc926b2ee8e
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-08-30 10:48:45 +0900

    [Update] Server 202208301048
    - etc

--- tannae-server/src/main/java/codesver/tannae/controller/ServiceController.java
+++ tannae-server/src/main/java/codesver/tannae/controller/ServiceController.java
@@ -1,5 +1,6 @@
 package codesver.tannae.controller;
 
+import codesver.tannae.domain.Process;
 import codesver.tannae.dto.ServiceRequestDTO;
 import codesver.tannae.service.RequestProcessor;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -23,7 +26,7 @@ public class ServiceController {
     @PostMapping("/request")
     public void request(@RequestBody ServiceRequestDTO dto) {
         log.info("[CONTROLLER-SERVICE : REQUEST] /service/request body={}", dto);
-
+        Optional<Process> process = processor.processRequest(dto);
 //        if (vehicle.isPresent()) {
 //            // Kakao api communication
 //            // Update database
--- tannae-server/src/main/java/codesver/tannae/service/RequestProcessor.java
+++ tannae-server/src/main/java/codesver/tannae/service/RequestProcessor.java
@@ -1,9 +1,14 @@
 package codesver.tannae.service;
 
+import codesver.tannae.domain.Process;
+import codesver.tannae.domain.Vehicle;
+import codesver.tannae.dto.ServiceRequestDTO;
 import codesver.tannae.repository.process.ProcessRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -11,4 +16,17 @@ import org.springframework.stereotype.Service;
 public class RequestProcessor {
 
     private final ProcessRepository processRepository;
+    private final VehicleFinder finder;
+
+    public Optional<Process> processRequest(ServiceRequestDTO dto) {
+        return dto.getShare() ? processShareRequest(dto) : processNonShareRequest(dto);
+    }
+
+    private Optional<Process> processShareRequest(ServiceRequestDTO dto) {
+        return Optional.empty();
+    }
+
+    private Optional<Process> processNonShareRequest(ServiceRequestDTO dto) {
+        Optional<Vehicle> vehicle = finder.findVehicle(dto);
+    }
 }
