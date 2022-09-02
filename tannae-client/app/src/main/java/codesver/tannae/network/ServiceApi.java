71477a8a02f865b865eb6d43d9fb04afa8754a51
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-09-02 10:55:50 +0900

    [Update] Server 202209021054
    - Change url

--- tannae-client/app/src/main/java/codesver/tannae/network/ServiceApi.java
+++ tannae-client/app/src/main/java/codesver/tannae/network/ServiceApi.java
@@ -35,6 +35,6 @@ public interface ServiceApi {
     @POST("/service/request")
     Call<ServiceResponseDTO> request(@Body ServiceRequestDTO dto);
 
-    @PATCH("/vehicles/{vsn}")
+    @PATCH("/vehicles/{vsn}/switch-run")
     Call<Boolean> switchRun(@Path("vsn") Integer vsn, @Query("run") Boolean run);
 }
