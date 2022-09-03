9e9554567de0a779b2f6951b3a52365327692d52
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-09-03 20:00:30 +0900

    [Update] Server 202209032000
    - etc

--- tannae-server/src/main/java/codesver/tannae/service/SummaryEditor.java
+++ tannae-server/src/main/java/codesver/tannae/service/SummaryEditor.java
@@ -15,7 +15,7 @@ public class SummaryEditor {
 
     public JSONObject createSummary(Vehicle vehicle, ServiceRequestDTO dto) {
         log.info("[SERVICE-SUMMARY-EDITOR : CREATE_SUMMARY] Creating new summary");
-        JSONObject origin = createPoint("vehicle", vehicle.getLongitude(), vehicle.getLatitude(), -1, 0, 0, false);
+        JSONObject origin = createPoint("차량 시작 지점", vehicle.getLongitude(), vehicle.getLatitude(), -1, 0, 0, false);
         JSONObject destination = createPoint(dto.getDestination(), dto.getDestinationLongitude(), dto.getDestinationLatitude(), dto.getUsn(), 0, 0, false);
         JSONObject waypoint = createPoint(dto.getOrigin(), dto.getOriginLongitude(), dto.getOriginLatitude(), dto.getUsn(), 0, 0, false);
         JSONArray waypoints = new JSONArray().put(waypoint);
