bc07a48c694313f7e403e48163c89b95c920c051
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-09-03 18:38:03 +0900

    [Update] Test 202209031837
    - Test class

---
+++ tannae-server/src/test/java/codesver/tannae/JavaTest.java
@@ -0,0 +1,12 @@
+package codesver.tannae;
+
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class JavaTest {
+
+
+}
