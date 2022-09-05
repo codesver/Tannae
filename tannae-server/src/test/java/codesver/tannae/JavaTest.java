5eae3094842eb750673ae7ca89611168e43e7c13
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-09-05 15:37:18 +0900

    [Update] Server 202209051537
    - test

--- tannae-server/src/test/java/codesver/tannae/JavaTest.java
+++ tannae-server/src/test/java/codesver/tannae/JavaTest.java
@@ -1,12 +1,13 @@
 package codesver.tannae;
 
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
 public class JavaTest {
 
-
+    @Test
+    void test() {
+        
+    }
 }
