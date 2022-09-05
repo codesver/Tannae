a5067556986b0e4d1e648cbfc595d413337aa8c1
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-09-05 16:48:50 +0900

    [Update] Server 202209051647
    - Create availableCoordinate
    - Create isInside
    - Create destinationIsFurther
    - Create isInsideAfterEndPath

--- tannae-server/src/main/java/codesver/tannae/service/ProcessFinder.java
+++ tannae-server/src/main/java/codesver/tannae/service/ProcessFinder.java
@@ -6,6 +6,8 @@ import codesver.tannae.dto.ServiceRequestDTO;
 import codesver.tannae.repository.process.ProcessRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,9 +26,13 @@ public class ProcessFinder {
             return new DRO<>(2);
         } else {
             sortProcessList(processes, dto.getOriginLatitude(), dto.getOriginLongitude());
-            
-
-
+            for (Process process : processes) {
+                if (availableCoordinate(dto, process)) {
+                    // Edit process path (add new request)
+                    // update vehicle num
+                    // update process path
+                }
+            }
             return null;
         }
     }
@@ -43,5 +49,59 @@ public class ProcessFinder {
             double lengthB = Math.pow(vehicleLatitudeB - originLatitude, 2) + Math.pow(vehicleLongitudeB - originLongitude, 2);
             return lengthA == lengthB ? 0 : (lengthA - lengthB < 0 ? -1 : 1);
         });
+    }
+
+    private boolean availableCoordinate(ServiceRequestDTO dto, Process process) {
+        log.info("[SERVICE-PROCESS-FINDER : AVAILABLE_COORDINATE] Check if requested coordinate is available in path={}", process.getPath());
+        JSONArray path = new JSONArray(process.getPath());
+        Double ox = dto.getOriginLongitude();
+        Double oy = dto.getOriginLatitude();
+        Double dx = dto.getDestinationLongitude();
+        Double dy = dto.getDestinationLatitude();
+
+        for (int i = 0; i < path.length() - 1; i++) {
+            JSONObject frontPoint = path.getJSONObject(i);
+            JSONObject backPoint = path.getJSONObject(i + 1);
+
+            double fx = frontPoint.getDouble("x");
+            double fy = frontPoint.getDouble("y");
+            double bx = backPoint.getDouble("x");
+            double by = backPoint.getDouble("y");
+
+            if (isInside(ox, oy, fx, fy, bx, by)) {
+                if (isInside(dx, dy, fx, fy, bx, by)) return destinationIsFurther(ox, oy, dx, dy, fx, fy);
+                else {
+                    for (int j = i + 1; j < path.length() - 1; j++) {
+                        frontPoint = path.getJSONObject(j);
+                        backPoint = path.getJSONObject(j + 1);
+
+                        fx = frontPoint.getDouble("x");
+                        fy = frontPoint.getDouble("y");
+                        bx = backPoint.getDouble("x");
+                        by = backPoint.getDouble("y");
+
+                        if (isInside(dx, dy, fx, fy, bx, by)) return true;
+                    }
+                    JSONObject end = path.getJSONObject(path.length() - 1);
+                    return isInsideAfterEndOfPath(dx, dy, end.getDouble("x"), end.getDouble("y"));
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean isInside(double x, double y, double fx, double fy, double bx, double by) {
+        log.info("[SERVICE-PROCESS-FINDER : IS_INSIDE] Check if point is inside of single path");
+        return true;
+    }
+
+    private boolean destinationIsFurther(double ox, double oy, double dx, double dy, double fx, double fy) {
+        log.info("[SERVICE-PROCESS-FINDER : DESTINATION_IS_FURTHER] Check if destination({}, {}) is further than origin({}, {}) from front point({}, {})", ox, oy, dx, dy, fx, fy);
+        return true;
+    }
+
+    private boolean isInsideAfterEndOfPath(double x, double y, double ex, double ey) {
+        log.info("[SERVICE-PROCESS-FINDER : IS_INSIDE_AFTER_END_OF_PATH] Check if destination({}, {}) is allowed after end point({}, {})", x, y, ex, ey);
+        return true;
     }
 }
