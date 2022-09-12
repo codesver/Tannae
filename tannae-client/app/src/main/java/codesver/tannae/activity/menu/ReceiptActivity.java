34486d63a1b86eb9f81dc3a5f32ed3c64afd4f2c
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-09-12 10:16:36 +0900

    [Update] Client 202209121016
    - activity_receipt

--- tannae-client/app/src/main/java/codesver/tannae/activity/menu/ReceiptActivity.java
+++ tannae-client/app/src/main/java/codesver/tannae/activity/menu/ReceiptActivity.java
@@ -1,6 +1,8 @@
 package codesver.tannae.activity.menu;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +10,25 @@ import codesver.tannae.R;
 
 public class ReceiptActivity extends AppCompatActivity {
 
+    private Button buttonBack, buttonCheckOrigin, buttonCheckDestination, buttonEvaluate;
+    private TextView textHsn, textDate, textRequestTime, textBoardTime, textArrivalTime, textOrigin, textDestination;
+    private TextView textOriginalDistance, textOriginalDuration, textOriginalFare;
+    private TextView textRealDistance, textRealDuration, textRealFare;
+    private TextView textFare, textSaleRatio, textLeftPoint;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receipt);
+        setViews();
+        setEventListeners();
+    }
+
+    private void setViews() {
+
+    }
+
+    private void setEventListeners() {
+
     }
 }
