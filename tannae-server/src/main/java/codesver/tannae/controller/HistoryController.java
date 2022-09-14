f2c0700bf5cdb7774a4794eae84508cae7ac320c
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-09-14 15:32:34 +0900

    [Update] Server 202209141532
    - etc

--- tannae-server/src/main/java/codesver/tannae/controller/HistoryController.java
+++ tannae-server/src/main/java/codesver/tannae/controller/HistoryController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 @Slf4j
