0d3c1d5260708cf6a9261b23ac036682ff1376fc
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-10 14:19:22 +0900

    [Update] Server 202210101419
    [Update] ContentController
    - add GET /contents

--- tannae-server/src/main/java/codesver/tannae/controller/ContentController.java
+++ tannae-server/src/main/java/codesver/tannae/controller/ContentController.java
@@ -1,13 +1,25 @@
 package codesver.tannae.controller;
 
+import codesver.tannae.domain.Content;
+import codesver.tannae.repository.content.ContentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/contents")
 public class ContentController {
+
+    private final ContentRepository contentRepository;
+
+    @GetMapping
+    public List<Content> getContents() {
+        return contentRepository.getContents();
+    }
 }
