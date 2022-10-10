2f9abc579e30b062f274515311d29df616ad7be9
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-10 14:10:49 +0900

    [Update] Client 202210101410
    [Update] QnaActivity
    - setting

--- tannae-client/app/src/main/java/codesver/tannae/activity/menu/QnaActivity.java
+++ tannae-client/app/src/main/java/codesver/tannae/activity/menu/QnaActivity.java
@@ -2,15 +2,29 @@ package codesver.tannae.activity.menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import codesver.tannae.R;
+import codesver.tannae.domain.History;
+import codesver.tannae.service.InnerDB;
+import codesver.tannae.service.ListViewAdapter;
 
 public class QnaActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ListViewAdapter<History> adapter;
+
+    private SharedPreferences getter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qna);
+
+        getter = InnerDB.getter(getApplicationContext());
+
+        listView = findViewById(R.id.list_view_qnas_qna);
     }
 }
