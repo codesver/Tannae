2a6c8d72e925936d1e71e432a9b5dc167d780ea0
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-10 15:53:05 +0900

    [Server-Update] ContentSpringDataJpaRepository 202210101552
    - change Long to Integer

--- tannae-server/src/main/java/codesver/tannae/repository/content/ContentSpringDataJpaRepository.java
+++ tannae-server/src/main/java/codesver/tannae/repository/content/ContentSpringDataJpaRepository.java
@@ -3,6 +3,6 @@ package codesver.tannae.repository.content;
 import codesver.tannae.domain.Content;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContentSpringDataJpaRepository extends JpaRepository<Content, Long> {
+public interface ContentSpringDataJpaRepository extends JpaRepository<Content, Integer> {
     
 }
