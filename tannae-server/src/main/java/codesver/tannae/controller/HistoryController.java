6f6d7f7bf2d43843d5cd00f8b9fc0910aa9e81c2
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-10 16:02:28 +0900

    [Server-Update] HistoryController 202210101602
    - Change History to HistoryDTO

--- tannae-server/src/main/java/codesver/tannae/controller/HistoryController.java
+++ tannae-server/src/main/java/codesver/tannae/controller/HistoryController.java
@@ -1,11 +1,13 @@
 package codesver.tannae.controller;
 
 import codesver.tannae.domain.History;
+import codesver.tannae.dto.HistoryDTO;
 import codesver.tannae.repository.history.HistoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -17,20 +19,25 @@ public class HistoryController {
     private final HistoryRepository historyRepository;
 
     @GetMapping
-    public List<History> getHistories(@RequestParam Integer usn) {
+    public List<HistoryDTO> getHistories(@RequestParam Integer usn) {
         log.info("[CONTROLLER-HISTORY {} : GET_HISTORIES] /histories?usn={}", Thread.currentThread().getId(), usn);
-        return historyRepository.findHistories(usn);
+        List<History> histories = historyRepository.findHistories(usn);
+        List<HistoryDTO> historyDTOS = new ArrayList<>();
+        for (History history : histories) historyDTOS.add(history.getDTO());
+        return historyDTOS;
     }
 
     @GetMapping("/{hsn}")
-    public History getReceiptByHsn(@PathVariable("hsn") Integer hsn) {
+    public HistoryDTO getReceiptByHsn(@PathVariable("hsn") Integer hsn) {
         log.info("[CONTROLLER-HISTORY {} : GET_RECEIPT_BY_HSN] /histories?hsn={}", Thread.currentThread().getId(), hsn);
-        return historyRepository.findHistoryByHsn(hsn);
+        History history = historyRepository.findHistoryByHsn(hsn);
+        return history.getDTO();
     }
 
     @GetMapping("/users")
-    public History getReceiptByUsn(@RequestParam Integer usn) {
+    public HistoryDTO getReceiptByUsn(@RequestParam Integer usn) {
         log.info("[CONTROLLER-HISTORY {} : GET_RECEIPT] /histories/users?usn={}", Thread.currentThread().getId(), usn);
-        return historyRepository.findHistoryByUsn(usn);
+        History history = historyRepository.findHistoryByUsn(usn);
+        return history.getDTO();
     }
 }
