3b148f16282eae8fe9e76b4d3c687e838469e5d4
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-10 14:07:06 +0900

    [Update] Server 202210101406
    [Create] ContentSpringDataJpaRepository

---
+++ tannae-server/src/main/java/codesver/tannae/repository/content/ContentSpringDataJpaRepository.java
@@ -0,0 +1,8 @@
+package codesver.tannae.repository.content;
+
+import codesver.tannae.domain.Content;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContentSpringDataJpaRepository extends JpaRepository<Content, Long> {
+    
+}
