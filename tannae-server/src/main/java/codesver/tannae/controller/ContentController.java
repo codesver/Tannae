752a780839f8cf4955505908b680548b7d59b6b7
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-10 16:31:11 +0900

    [Server-Update] ContentController 202210101630
    - Convert content list to content dto list
    - return content dto list

--- tannae-server/src/main/java/codesver/tannae/controller/ContentController.java
+++ tannae-server/src/main/java/codesver/tannae/controller/ContentController.java
@@ -1,6 +1,7 @@
 package codesver.tannae.controller;
 
 import codesver.tannae.domain.Content;
+import codesver.tannae.dto.ContentDTO;
 import codesver.tannae.repository.content.ContentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -19,8 +21,11 @@ public class ContentController {
     private final ContentRepository contentRepository;
 
     @GetMapping
-    public List<Content> getContents() {
+    public List<ContentDTO> getContents() {
         log.info("[CONTROLLER-CONTENT {} : GET_CONTENTS] /contents", Thread.currentThread().getId());
-        return contentRepository.getContents();
+        List<Content> contents = contentRepository.getContents();
+        List<ContentDTO> contentDTOS = new ArrayList<>();
+        for (Content content : contents) contentDTOS.add(content.getDTO());
+        return contentDTOS;
     }
 }
