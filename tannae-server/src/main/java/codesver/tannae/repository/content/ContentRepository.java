030b2a91b6d6509d516b4cab1fbc11c3edd43e07
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-10 14:17:00 +0900

    [Update] Server 202210101416
    [Update] ContentRepository
    - add getContetns

--- tannae-server/src/main/java/codesver/tannae/repository/content/ContentRepository.java
+++ tannae-server/src/main/java/codesver/tannae/repository/content/ContentRepository.java
@@ -1,4 +1,10 @@
 package codesver.tannae.repository.content;
 
+import codesver.tannae.domain.Content;
+
+import java.util.List;
+
 public interface ContentRepository {
+
+    public List<Content> getContents();
 }
