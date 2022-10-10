2c1b26ac90fa4b3e55e581e72cd288800f3c0530
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-10 14:28:03 +0900

    [Update] Client 202210101427
    [Update] QnaActivity
    - Implement inner Network

--- tannae-client/app/src/main/java/codesver/tannae/activity/menu/QnaActivity.java
+++ tannae-client/app/src/main/java/codesver/tannae/activity/menu/QnaActivity.java
@@ -14,6 +14,7 @@ import codesver.tannae.domain.History;
 import codesver.tannae.network.Network;
 import codesver.tannae.service.InnerDB;
 import codesver.tannae.service.ListViewAdapter;
+import codesver.tannae.service.Toaster;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -21,7 +22,7 @@ import retrofit2.Response;
 public class QnaActivity extends AppCompatActivity {
 
     private ListView listView;
-    private ListViewAdapter<History> adapter;
+    private ListViewAdapter<Content> adapter;
 
     private SharedPreferences getter;
 
@@ -41,12 +42,20 @@ public class QnaActivity extends AppCompatActivity {
         Network.service.getContents().enqueue(new Callback<List<Content>>() {
             @Override
             public void onResponse(Call<List<Content>> call, Response<List<Content>> response) {
-                
+                List<Content> contents = response.body();
+                if (contents.isEmpty()) {
+                    Toaster.toast(getApplicationContext(), "QnA가 없습니다.");
+                    onBackPressed();
+                } else {
+                    adapter = new ListViewAdapter<>();
+                    for (Content content : contents) adapter.addItem(content);
+                    listView.setAdapter(adapter);
+                }
             }
 
             @Override
             public void onFailure(Call<List<Content>> call, Throwable t) {
-
+                Toaster.toast(getApplicationContext(), "오류가 발생했습니다.\n고객센터로 문의바랍니다.");
             }
         });
     }
