bb072d4bb2c88d1b9502d9da9157418cb9a6502f
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-10 16:02:05 +0900

    [Server-Create] HistoryDTO 202210101601
    - DTO for history entity

---
+++ tannae-server/src/main/java/codesver/tannae/dto/HistoryDTO.java
@@ -0,0 +1,28 @@
+package codesver.tannae.dto;
+
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+public class HistoryDTO {
+
+    private Integer hsn;
+    private String origin;
+    private Double originLatitude;
+    private Double originLongitude;
+    private String destination;
+    private Double destinationLatitude;
+    private Double destinationLongitude;
+    private Boolean share;
+    private LocalDateTime requestTime;
+    private LocalDateTime boardingTime;
+    private LocalDateTime arrivalTime;
+    private Integer originalFare;
+    private Integer originalDistance;
+    private Integer originalDuration;
+    private Integer realFare;
+    private Integer realDistance;
+    private Integer realDuration;
+    private Integer usn;
+}
