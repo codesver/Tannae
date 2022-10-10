0b4b5d7fd1f06d3993b4507bad7fadc06844e989
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-10 14:14:40 +0900

    [Update] Client 202210101414
    [Update] QnaActivity
    - Add setAdapter structure

--- tannae-client/app/src/main/java/codesver/tannae/activity/menu/QnaActivity.java
+++ tannae-client/app/src/main/java/codesver/tannae/activity/menu/QnaActivity.java
@@ -6,10 +6,17 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.util.List;
+
 import codesver.tannae.R;
+import codesver.tannae.domain.Content;
 import codesver.tannae.domain.History;
+import codesver.tannae.network.Network;
 import codesver.tannae.service.InnerDB;
 import codesver.tannae.service.ListViewAdapter;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class QnaActivity extends AppCompatActivity {
 
@@ -26,5 +33,21 @@ public class QnaActivity extends AppCompatActivity {
         getter = InnerDB.getter(getApplicationContext());
 
         listView = findViewById(R.id.list_view_qnas_qna);
+
+        setAdapter();
+    }
+
+    private void setAdapter() {
+        Network.service.getContents().enqueue(new Callback<List<Content>>() {
+            @Override
+            public void onResponse(Call<List<Content>> call, Response<List<Content>> response) {
+                
+            }
+
+            @Override
+            public void onFailure(Call<List<Content>> call, Throwable t) {
+
+            }
+        });
     }
 }
