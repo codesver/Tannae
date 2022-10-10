8dfc5ef27ff4c507193c875113eb7a0f15815f82
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-10 14:18:25 +0900

    [Update] Server 202210101418
    [Update] ContentJpaRepository
    - Implement getContents

--- tannae-server/src/main/java/codesver/tannae/repository/content/ContentJpaRepository.java
+++ tannae-server/src/main/java/codesver/tannae/repository/content/ContentJpaRepository.java
@@ -1,13 +1,24 @@
 package codesver.tannae.repository.content;
 
+import codesver.tannae.domain.Content;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Slf4j
 @Repository
 @Transactional
 @RequiredArgsConstructor
-public class ContentJpaRepository {
+public class ContentJpaRepository implements ContentRepository {
+
+    private final ContentSpringDataJpaRepository repository;
+
+    @Override
+    public List<Content> getContents() {
+        List<Content> contents = repository.findAll();
+        return contents;
+    }
 }
