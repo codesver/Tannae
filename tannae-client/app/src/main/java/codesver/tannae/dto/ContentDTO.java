e8a0b735e19b203942a3f84ac29c4595512f0f88
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-10 16:32:04 +0900

    [Client-Create] ContentDTO 202210101631
    - create dto of content entity

---
+++ tannae-client/app/src/main/java/codesver/tannae/dto/ContentDTO.java
@@ -0,0 +1,40 @@
+package codesver.tannae.dto;
+
+public class ContentDTO {
+
+    private Integer csn;
+    private String title;
+    private String question;
+    private String answer;
+    private String dateTime;
+    private Boolean faq;
+    private Integer usn;
+
+    public Integer getCsn() {
+        return csn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public Boolean getFaq() {
+        return faq;
+    }
+
+    public Integer getUsn() {
+        return usn;
+    }
+}
