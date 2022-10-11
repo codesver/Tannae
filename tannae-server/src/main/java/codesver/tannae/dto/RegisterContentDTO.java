0ea8cc9c14ab13867b99a272ee9a0ad4f3a7326c
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-11 15:57:55 +0900

    [Server-Update] RegisterContentDTO 202210111557
    - add converting method

--- tannae-server/src/main/java/codesver/tannae/dto/RegisterContentDTO.java
+++ tannae-server/src/main/java/codesver/tannae/dto/RegisterContentDTO.java
@@ -1,6 +1,10 @@
 package codesver.tannae.dto;
 
+import codesver.tannae.domain.Content;
+import codesver.tannae.domain.User;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 public class RegisterContentDTO {
@@ -8,4 +12,9 @@ public class RegisterContentDTO {
     private Integer usn;
     private String title;
     private String question;
+
+    public Content convertToEntity() {
+        User user = new User(usn);
+        return new Content(title, question, LocalDateTime.now(), user);
+    }
 }
