1daa81e36cc4dc10aca9b442158e5130726902ea
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-11 14:43:33 +0900

    [DB-Update] Content 202210111443
    - change max length of question up to 100

--- tannae-server/src/main/java/codesver/tannae/domain/Content.java
+++ tannae-server/src/main/java/codesver/tannae/domain/Content.java
@@ -21,7 +21,7 @@ public class Content {
     @Column(length = 10, nullable = false)
     private String title;
 
-    @Column(length = 50, nullable = false)
+    @Column(length = 100, nullable = false)
     private String question;
 
     @Column(length = 50)
