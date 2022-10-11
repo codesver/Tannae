7bd15fd1f6fb33cc2eea75ce101af9768e8cd758
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-11 14:47:19 +0900

    [Client-Update] QnaRegister 202210111447
    - add view field
    - add setViews method

--- tannae-client/app/src/main/java/codesver/tannae/activity/menu/qna/QnaRegisterActivity.java
+++ tannae-client/app/src/main/java/codesver/tannae/activity/menu/qna/QnaRegisterActivity.java
@@ -1,6 +1,8 @@
 package codesver.tannae.activity.menu.qna;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +10,20 @@ import codesver.tannae.R;
 
 public class QnaRegisterActivity extends AppCompatActivity {
 
+    private EditText editTitle, editQuestion;
+    private Button buttonRegister, buttonBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qna_register);
+        setViews();
+    }
+
+    private void setViews() {
+        editTitle = findViewById(R.id.edit_title_qna_register);
+        editQuestion = findViewById(R.id.edit_question_qna_register);
+        buttonRegister = findViewById(R.id.button_register_qna_register);
+        buttonBack = findViewById(R.id.button_back_qna_register);
     }
 }
