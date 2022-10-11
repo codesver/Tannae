755ed12ff2160965bc1f8922fce645eb19e40eb5
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-11 15:58:29 +0900

    [Service-Fix] ContentService 202210111558
    - change parameter

--- tannae-server/src/main/java/codesver/tannae/service/domain/ContentService.java
+++ tannae-server/src/main/java/codesver/tannae/service/domain/ContentService.java
@@ -1,7 +1,7 @@
 package codesver.tannae.service.domain;
 
 import codesver.tannae.domain.Content;
-import codesver.tannae.dto.ContentDTO;
+import codesver.tannae.dto.RegisterContentDTO;
 import codesver.tannae.repository.content.ContentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,7 +14,7 @@ public class ContentService {
 
     private final ContentRepository contentDAO;
 
-    public Boolean register(ContentDTO dto) {
+    public Boolean register(RegisterContentDTO dto) {
         log.info("[SERVICE-CONTENT {} : REGISTER] DTO={}", Thread.currentThread().getId(), dto);
         Content content = dto.convertToEntity();
         Integer csn = contentDAO.register(content);
