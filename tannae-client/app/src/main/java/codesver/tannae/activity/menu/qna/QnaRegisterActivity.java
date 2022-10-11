7aedbc6e2c93e24053cbd8e54254a3983dc11394
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-11 14:18:17 +0900

    [Client-Create] QnaRegisterActivity 202210111418
    - create activity

---
+++ tannae-client/app/src/main/java/codesver/tannae/activity/menu/qna/QnaRegisterActivity.java
@@ -0,0 +1,16 @@
+package codesver.tannae.activity.menu.qna;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import codesver.tannae.R;
+
+public class QnaRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qna_register);
+    }
+}
