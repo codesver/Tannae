1baf761bc5fe3c592cc18aa407da0122f001145d
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-11 15:19:41 +0900

    [Server-Create] RegisterContentDTO 202210111518
    - create DTO for registering content
    - client to server

---
+++ tannae-server/src/main/java/codesver/tannae/dto/RegisterContentDTO.java
@@ -0,0 +1,11 @@
+package codesver.tannae.dto;
+
+import lombok.Getter;
+
+@Getter
+public class RegisterContentDTO {
+
+    private Integer usn;
+    private String title;
+    private String question;
+}
