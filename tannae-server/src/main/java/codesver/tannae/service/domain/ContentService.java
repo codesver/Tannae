8e7354e90507de6183ee3fc27c55a95f442620c1
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-11 15:53:05 +0900

    [Server-Update] ContentService 202210111551
    - add register method

--- tannae-server/src/main/java/codesver/tannae/service/domain/ContentService.java
+++ tannae-server/src/main/java/codesver/tannae/service/domain/ContentService.java
@@ -1,9 +1,24 @@
 package codesver.tannae.service.domain;
 
+import codesver.tannae.domain.Content;
+import codesver.tannae.dto.ContentDTO;
+import codesver.tannae.repository.content.ContentRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class ContentService {
+
+    private final ContentRepository contentDAO;
+
+    public Boolean register(ContentDTO dto) {
+        log.info("[SERVICE-CONTENT {} : REGISTER] DTO={}", Thread.currentThread().getId(), dto);
+        Content content = dto.convertToEntity();
+        Integer csn = contentDAO.register(content);
+        log.info("[SERVICE-CONTENT {} : REGISTER_FINISH] CSN={}", Thread.currentThread().getId(), csn);
+        return true;
+    }
 }
