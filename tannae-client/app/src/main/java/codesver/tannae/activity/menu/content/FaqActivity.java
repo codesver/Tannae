af4f67e739930936c98a7c576671ef3e4cc5124a
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-13 18:43:57 +0900

    [Client-Create] Activity 202210131843
    - create faq activity

---
+++ tannae-client/app/src/main/java/codesver/tannae/activity/menu/content/FaqActivity.java
@@ -0,0 +1,14 @@
+package codesver.tannae.activity.menu.content;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FaqActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+    }
+}
