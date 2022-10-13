e052faee8c5d5a894b0ba9b700ad0ee841c3db2e
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-13 18:47:36 +0900

    [Client-Update] FaqActivity 202210131847
    - add back button event listener

--- tannae-client/app/src/main/java/codesver/tannae/activity/menu/content/FaqActivity.java
+++ tannae-client/app/src/main/java/codesver/tannae/activity/menu/content/FaqActivity.java
@@ -1,14 +1,24 @@
 package codesver.tannae.activity.menu.content;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import codesver.tannae.R;
+import codesver.tannae.dto.ContentDTO;
+import codesver.tannae.service.ListViewAdapter;
 
 public class FaqActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ListViewAdapter<ContentDTO> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
+        findViewById(R.id.button_back_faq_activity).setOnClickListener(v -> onBackPressed());
     }
 }
