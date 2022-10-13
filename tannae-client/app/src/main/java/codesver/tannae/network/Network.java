64a571439c287db0f0da3c9771b5f0c2cda26d19
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-13 10:53:10 +0900

    [Client-Update] Network 202210131052
    - change ip address to oracle ip

--- tannae-client/app/src/main/java/codesver/tannae/network/Network.java
+++ tannae-client/app/src/main/java/codesver/tannae/network/Network.java
@@ -4,6 +4,6 @@ import ua.naiksoftware.stomp.StompClient;
 
 public class Network {
     public static ServiceApi service;
-    public static final String ip = "168.131.175.90:8080"; // AWS 15.164.225.233:8080
+    public static final String ip = "150.230.252.172:8080"; // AWS 15.164.225.233:8080 ORACLE 150.230.252.172:8080
     public static StompClient stomp;
 }
