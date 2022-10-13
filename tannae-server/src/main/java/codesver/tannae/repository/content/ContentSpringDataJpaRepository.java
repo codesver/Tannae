a049e855eb912964538748fb9316ccf34d1f5d5e
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-13 19:22:50 +0900

    [Server-Update] ContentSpringDataJpaRepository 202210131922

--- tannae-server/src/main/java/codesver/tannae/repository/content/ContentSpringDataJpaRepository.java
+++ tannae-server/src/main/java/codesver/tannae/repository/content/ContentSpringDataJpaRepository.java
@@ -3,6 +3,9 @@ package codesver.tannae.repository.content;
 import codesver.tannae.domain.Content;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ContentSpringDataJpaRepository extends JpaRepository<Content, Integer> {
-    
+
+    List<Content> findContentByFaqIsTrue();
 }
