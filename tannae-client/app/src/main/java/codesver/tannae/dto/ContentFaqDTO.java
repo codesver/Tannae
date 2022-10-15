145cfd66c2d576e12feee53d32658337b2a5f248
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-15 20:16:01 +0900

    [Client-Create] ContentFaqDTO 202210152014
    - create dto for faq

---
+++ tannae-client/app/src/main/java/codesver/tannae/dto/ContentFaqDTO.java
@@ -0,0 +1,35 @@
+package codesver.tannae.dto;
+
+public class ContentFaqDTO {
+
+    private Integer csn;
+    private String title;
+    private String question;
+    private String answer;
+    private String dateTime;
+    private Integer usn;
+
+    public Integer getCsn() {
+        return csn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public Integer getUsn() {
+        return usn;
+    }
+}
