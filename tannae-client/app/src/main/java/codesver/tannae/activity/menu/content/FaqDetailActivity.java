4b6f0cf242e546635bc559520c15e72499c90019
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-15 21:08:30 +0900

    [Client-Update] FaqDetailActivity 202210152108
    - implement

--- tannae-client/app/src/main/java/codesver/tannae/activity/menu/content/FaqDetailActivity.java
+++ tannae-client/app/src/main/java/codesver/tannae/activity/menu/content/FaqDetailActivity.java
@@ -1,10 +1,18 @@
 package codesver.tannae.activity.menu.content;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import codesver.tannae.R;
+import codesver.tannae.dto.ContentDTO;
+import codesver.tannae.network.Network;
+import codesver.tannae.service.Toaster;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class FaqDetailActivity extends AppCompatActivity {
 
@@ -12,5 +20,29 @@ public class FaqDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq_detail);
+        findViewById(R.id.button_back_faq_detail_activity).setOnClickListener(v -> onBackPressed());
+        setTextByServer();
+    }
+
+    private void setTextByServer() {
+        Network.service.getContent(getIntent().getIntExtra("csn", 0)).enqueue(new Callback<ContentDTO>() {
+            @Override
+            public void onResponse(@NonNull Call<ContentDTO> call, @NonNull Response<ContentDTO> response) {
+                ContentDTO faq = response.body();
+                setTexts(faq);
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ContentDTO> call, @NonNull Throwable t) {
+                Toaster.toast(getApplicationContext(), "오류가 발생했습니다.\n고객센터로 문의바랍니다.");
+            }
+        });
+    }
+
+    private void setTexts(ContentDTO faq) {
+        ((TextView) findViewById(R.id.text_title_faq_detail)).setText(faq.getTitle());
+        ((TextView) findViewById(R.id.text_date_faq_detail)).setText(faq.getDateTime());
+        ((TextView) findViewById(R.id.text_question_faq_detail)).setText(faq.getQuestion());
+        ((TextView) findViewById(R.id.text_answer_faq_detail)).setText(faq.getAnswer());
     }
 }
