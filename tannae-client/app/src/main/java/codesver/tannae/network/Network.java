e3081faa0a0e73acf19d7cf20f32363b194d3cda
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-10-24 15:40:42 +0900

    [Client-Update] Network 202210241540
    - change ip address

--- tannae-client/app/src/main/java/codesver/tannae/network/Network.java
+++ tannae-client/app/src/main/java/codesver/tannae/network/Network.java
@@ -4,6 +4,6 @@ import ua.naiksoftware.stomp.StompClient;
 
 public class Network {
     public static ServiceApi service;
-    public static final String ip = "150.230.252.172:8080";
+    public static final String ip = "13.124.236.45:8080";
     public static StompClient stomp;
 }
