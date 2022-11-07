cac07849d66392bdb87651e70003f2d86505a830
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-07 19:37:19 +0900

    feat(onCreate): set list view and event listener of back button

--- tannae-client/app/src/main/java/codesver/tannae/activity/menu/lost/LostActivity.java
+++ tannae-client/app/src/main/java/codesver/tannae/activity/menu/lost/LostActivity.java
@@ -1,16 +1,26 @@
 package codesver.tannae.activity.menu.lost;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import codesver.tannae.R;
+import codesver.tannae.dto.ContentFaqDTO;
+import codesver.tannae.dto.LostDTO;
+import codesver.tannae.service.ListViewAdapter;
 
 public class LostActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ListViewAdapter<LostDTO> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lost);
+
+        listView = findViewById(R.id.list_view_losts_lost);
+        findViewById(R.id.button_back_lost).setOnClickListener(v -> onBackPressed());
     }
 }
