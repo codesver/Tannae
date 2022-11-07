3aba2a39e27629b8ee4b4bdc02300cb8d75d50a7
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-07 20:11:30 +0900

    feat-server(LostController): get all losts

--- tannae-server/src/main/java/codesver/tannae/controller/LostController.java
+++ tannae-server/src/main/java/codesver/tannae/controller/LostController.java
@@ -1,14 +1,16 @@
 package codesver.tannae.controller;
 
+import codesver.tannae.domain.Lost;
 import codesver.tannae.dto.LostDTO;
+import codesver.tannae.repository.lost.LostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -16,9 +18,12 @@ import java.util.List;
 @RequestMapping("/losts")
 public class LostController {
 
+    private final LostRepository lostRepository;
+
     @GetMapping
     public List<LostDTO> getLosts() {
-        // service
-        return new ArrayList<>();
+        return lostRepository.findAll().stream()
+                .map(lost -> new LostDTO(lost.getLsn(), lost.getLost(), lost.getLostDate().toString(), lost.getVehicle().getVrn()))
+                .collect(Collectors.toList());
     }
 }
--- tannae-server/src/main/java/codesver/tannae/dto/LostDTO.java
+++ tannae-server/src/main/java/codesver/tannae/dto/LostDTO.java
@@ -1,14 +1,16 @@
 package codesver.tannae.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class LostDTO {
 
     private Integer lsn;
     private String lost;
     private String date;
-    private Integer vrn;
+    private String vrn;
 }
