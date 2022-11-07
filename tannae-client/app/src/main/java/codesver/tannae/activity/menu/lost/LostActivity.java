174ee962a3d9278997633db59e3a21de923b64d3
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-07 18:48:16 +0900

    style(activity_lost): create lost activity ui

---
+++ tannae-client/app/src/main/java/codesver/tannae/activity/menu/lost/LostActivity.java
@@ -0,0 +1,16 @@
+package codesver.tannae.activity.menu.lost;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import codesver.tannae.R;
+
+public class LostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lost);
+    }
+}
