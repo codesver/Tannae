1b6c61586dd3c7125499964e8b72f41c5c05af38
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-07 19:38:20 +0900

    feat(LostActivity): build command flow of setAdapter

--- tannae-client/app/src/main/java/codesver/tannae/activity/menu/lost/LostActivity.java
+++ tannae-client/app/src/main/java/codesver/tannae/activity/menu/lost/LostActivity.java
@@ -22,5 +22,10 @@ public class LostActivity extends AppCompatActivity {
 
         listView = findViewById(R.id.list_view_losts_lost);
         findViewById(R.id.button_back_lost).setOnClickListener(v -> onBackPressed());
+        setAdapter();
+    }
+
+    private void setAdapter() {
+        // Network
     }
 }
