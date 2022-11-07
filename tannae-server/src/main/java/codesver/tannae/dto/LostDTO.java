ebb0ab404cb23701ef9390660de714470489a5fa
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-07 19:54:46 +0900

    feat-server(LostDTO): create dto for lost table

---
+++ tannae-server/src/main/java/codesver/tannae/dto/LostDTO.java
@@ -0,0 +1,14 @@
+package codesver.tannae.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LostDTO {
+
+    private Integer lsn;
+    private String lost;
+    private String date;
+    private Integer vrn;
+}
