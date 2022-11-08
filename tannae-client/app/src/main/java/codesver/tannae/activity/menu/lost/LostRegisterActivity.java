0e97644f2da0850837bea0c755bf32bae24af368
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-08 13:53:48 +0900

    feat(LostRefisterActivity): implement

--- tannae-client/app/src/main/java/codesver/tannae/activity/menu/lost/LostRegisterActivity.java
+++ tannae-client/app/src/main/java/codesver/tannae/activity/menu/lost/LostRegisterActivity.java
@@ -1,6 +1,7 @@
 package codesver.tannae.activity.menu.lost;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +9,22 @@ import codesver.tannae.R;
 
 public class LostRegisterActivity extends AppCompatActivity {
 
+    private EditText editLost;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lost_register);
+        setViews();
+    }
+
+    private void setViews() {
+        editLost = findViewById(R.id.edit_lost_lost_register);
+        findViewById(R.id.button_back_lost_register).setOnClickListener(v -> onBackPressed());
+        findViewById(R.id.button_register_lost_register).setOnClickListener(v -> registerByServer());
+    }
+
+    private void registerByServer() {
+
     }
 }
