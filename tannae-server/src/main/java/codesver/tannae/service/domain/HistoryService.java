dcfdb21308855730a0bb18d41ed0cb380defdebe
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-08 16:31:12 +0900

    fix-server: manage transaction

--- tannae-server/src/main/java/codesver/tannae/service/domain/HistoryService.java
+++ tannae-server/src/main/java/codesver/tannae/service/domain/HistoryService.java
@@ -26,6 +26,7 @@ public class HistoryService {
         return historyRepository.findHistoryByHsn(hsn).getDTO();
     }
 
+    @Transactional
     public HistoryDTO getReceiptOfUser(Integer usn) {
         return historyRepository.findHistoryByUsn(usn).getDTO();
     }
