c71cb0245b7978628ce2e8ee45b8b42aacbd1c07
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-08 13:40:09 +0900

    style(activity_lost_register): create lost register ui

---
+++ tannae-client/app/src/main/java/codesver/tannae/activity/menu/lost/LostRegisterActivity.java
@@ -0,0 +1,16 @@
+package codesver.tannae.activity.menu.lost;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import codesver.tannae.R;
+
+public class LostRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lost_register);
+    }
+}
